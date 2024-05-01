b785d69d5dfcce61ec96156b369ca27719015401
Trent Shumay <trent@iotdesignshop.com>
2024-05-01 16:44:42 -0700

    Sim config

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -10,10 +10,11 @@ from launch.substitutions import Command
 def generate_launch_description():
 
     descr_path = get_package_share_path('dexhand_description')
-    default_model_path = descr_path / 'urdf/dexhand.xacro'
-    
-    gesture_path = get_package_share_path('dexhand_gesture_controller')
-    rviz_path = gesture_path / 'rviz/simulation.rviz'
+    default_model_path = descr_path / 'urdf/dexhand-left.xacro'
+
+    dexhand_manus_path = get_package_share_path('dexhand_manus')
+    rviz_path = dexhand_manus_path / 'rviz/simulation.rviz'
+
 
     urdf = ParameterValue(Command(['xacro ', str(default_model_path)]),
                                      value_type=str)
@@ -26,11 +27,11 @@ def generate_launch_description():
             name='robot_state_publisher',
             output='screen',
             parameters=[{'robot_description': urdf}]),
-        #Node(
-        #    package='dexhand_gesture_controller',
-        #    executable='gesture_controller',
-        #    name='gesture_controller',
-        #    output='screen'),
+        Node(
+            package='manus_ros2',
+            executable='manus_ros2',
+            name='manus_ros2',
+            output='screen'),
         Node(
             package='rviz2',
             executable='rviz2',
